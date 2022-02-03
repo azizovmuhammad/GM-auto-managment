ff8c0cd9c05e5539402b96901fca46321faa7091
azizovmukhammad <azizovmuhammaduz@gmail.com>
2022-02-03 08:36:49 +0500

    Initial commit

---
+++ src/main/java/com/example/GMautomanagment/GmAutoManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.GMautomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmAutoManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmAutoManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GMautomanagment/GmAutoManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GMautomanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmAutoManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
